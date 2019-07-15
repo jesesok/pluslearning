ae664c2194cf229d044683a74ae7abaaea799305
jesesok <jeses47@gmail.com>
2019-07-15 22:58:59 +0300

    zxccv

---
+++ cmake-build-debug/CMakeFiles/pluselearning.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pluselearning.dir/main.cpp.obj"
+  "pluselearning.pdb"
+  "pluselearning.exe"
+  "pluselearning.exe.manifest"
+  "libpluselearning.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pluselearning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
